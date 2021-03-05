c55090cf37a4f518b899277ee6e4905809b97ae9
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-03-04 20:57:45 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,48 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	downloadFileName = "index.html"
+	downloadURLPath  = "http://127.0.0.1"
+)
 
 func main() {
+	hostHTTPContent()
+	downloadHTTPContent()
+}
+
+func hostHTTPContent() {
 	http.Handle("/", http.FileServer(http.Dir("www")))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
+}
+
+func downloadHTTPContent() {
+	for {
+		err := downloadFile(downloadFileName, downloadURLPath)
+		if err != nil {
+			log.Println(err)
+		}
+		os.Remove(downloadFileName)
+	}
+}
+
+func downloadFile(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
