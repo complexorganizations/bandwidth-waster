00cd0afb720bfc054f7e04e7939e3528b47cf35d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-03-04 22:27:47 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-        "fmt"
-        "io"
-        "log"
-        "net/http"
-        "os"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 var (
-        megabytesToWaste = 102400000
-        downloadFileName = "index.html"
-        downloadURLPath  = "https://ros-static.ga/public/ros-data-waster-dummy"
+	megabytesToWaste = 102400000
+	downloadFileName = "index.html"
+	downloadURLPath  = "https://ros-static.ga/public/ros-data-waster-dummy"
 )
 
 func main() {
-        downloadHTTPContent()
+	downloadHTTPContent()
 }
 
 func downloadHTTPContent() {
-        for loop := 0; loop <= megabytesToWaste; loop++ {
-                err := downloadFile(downloadFileName, downloadURLPath)
-                if err != nil {
-                        log.Println(err)
-                }
-                os.Remove(downloadFileName)
-                fmt.Println(loop, "Megabytes Wasted")
-        }
+	for loop := 0; loop <= megabytesToWaste; loop++ {
+		err := downloadFile(downloadFileName, downloadURLPath)
+		if err != nil {
+			log.Println(err)
+		}
+		os.Remove(downloadFileName)
+		fmt.Println(loop, "Megabytes Wasted")
+	}
 }
 
 func downloadFile(filepath string, url string) error {
-        resp, err := http.Get(url)
-        if err != nil {
-                return err
-        }
-        defer resp.Body.Close()
-        out, err := os.Create(filepath)
-        if err != nil {
-                return err
-        }
-        defer out.Close()
-        _, err = io.Copy(out, resp.Body)
-        return err
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
