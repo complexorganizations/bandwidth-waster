61a4b81349d77cbebeac19f6eab071c773d47238
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-03-06 12:07:10 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"io"
 	"log"
@@ -9,13 +10,55 @@ import (
 	"time"
 )
 
+var (
+	arguments        = os.Args[1]
+	downloadFileName = randomString(1024)
+	downloadFileURL  = "https://raw.githubusercontent.com/complexorganizations/bandwidth-waster/main/random-test-file"
+)
+
 func main() {
-	downloadHTTPContent()
+	chooseUploadORDownload()
+}
+
+func chooseUploadORDownload() {
+	switch imageFilePath {
+	case "--download":
+		downloadHTTPContent()
+	case "--upload":
+		uploadHTTPContent()
+	default:
+		fmt.Println("Error: format not supported")
+	}
+}
+
+func uploadHTTPContent() {
+	for {
+		if !fileExists(downloadFileName) {
+			err := downloadFile(downloadFileName, downloadFileURL)
+			if err != nil {
+				log.Println(err)
+			}
+		} else if fileExists(downloadFileName) {
+			file, err := os.Open(downloadFileName)
+			if err != nil {
+				log.Println(err)
+			}
+			defer file.Close()
+			req, err := http.NewRequest("POST", "https://bashupload.com/", file)
+			if err != nil {
+				log.Println(err)
+			}
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Println(err)
+			}
+			defer resp.Body.Close()
+		}
+	}
 }
 
 func downloadHTTPContent() {
-	downloadFileName := "random-test-file"
-	downloadFileURL := "https://raw.githubusercontent.com/complexorganizations/bandwidth-waster/main/random-test-file"
 	startTime := time.Now()
 	for {
 		err := downloadFile(downloadFileName, downloadFileURL)
@@ -40,4 +83,19 @@ func downloadFile(filepath string, url string) error {
 	defer out.Close()
 	_, err = io.Copy(out, resp.Body)
 	return err
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func randomString(bytesSize int) string {
+	randomBytes := make([]byte, bytesSize)
+	rand.Read(randomBytes)
+	randomString := fmt.Sprintf("%X", randomBytes)
+	return randomString
 }
