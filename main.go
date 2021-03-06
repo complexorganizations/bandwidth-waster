4b3652b9ca028e10d44d2be5e9f2b659859f4af7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-03-06 10:29:14 -0500

    Update main.go

--- main.go
+++ main.go
@@ -13,61 +13,6 @@ func main() {
 	downloadHTTPContent()
 }
 
-func changeFileViaHour() string {
-	currentHour := time.Now().Hour()
-	var downloadURLPath string
-	if currentHour == 1 {
-		downloadURLPath = "https://sgp-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 2 {
-		downloadURLPath = "https://speed.hetzner.de/1GB.bin"
-	} else if currentHour == 3 {
-		downloadURLPath = "https://fastest.fish/lib/downloads/1GiB.bin"
-	} else if currentHour == 4 {
-		downloadURLPath = ""
-	} else if currentHour == 5 {
-		downloadURLPath = ""
-	} else if currentHour == 6 {
-		downloadURLPath = ""
-	} else if currentHour == 7 {
-		downloadURLPath = ""
-	} else if currentHour == 8 {
-		downloadURLPath = ""
-	} else if currentHour == 9 {
-		downloadURLPath = ""
-	} else if currentHour == 10 {
-		downloadURLPath = ""
-	} else if currentHour == 11 {
-		downloadURLPath = ""
-	} else if currentHour == 12 {
-		downloadURLPath = ""
-	} else if currentHour == 13 {
-		downloadURLPath = ""
-	} else if currentHour == 14 {
-		downloadURLPath = ""
-	} else if currentHour == 15 {
-		downloadURLPath = ""
-	} else if currentHour == 16 {
-		downloadURLPath = ""
-	} else if currentHour == 17 {
-		downloadURLPath = ""
-	} else if currentHour == 18 {
-		downloadURLPath = ""
-	} else if currentHour == 19 {
-		downloadURLPath = ""
-	} else if currentHour == 20 {
-		downloadURLPath = ""
-	} else if currentHour == 21 {
-		downloadURLPath = ""
-	} else if currentHour == 22 {
-		downloadURLPath = ""
-	} else if currentHour == 23 {
-		downloadURLPath = ""
-	} else if currentHour == 24 {
-		downloadURLPath = ""
-	}
-	return downloadURLPath
-}
-
 func downloadHTTPContent() {
 	downloadFileName := "delete-this-file"
 	startTime := time.Now()
@@ -94,4 +39,59 @@ func downloadFile(filepath string, url string) error {
 	defer out.Close()
 	_, err = io.Copy(out, resp.Body)
 	return err
+}
+
+func changeFileViaHour() string {
+	currentHour := time.Now().Hour()
+	var downloadURLPath string
+	if currentHour == 1 {
+		downloadURLPath = "https://sgp-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 2 {
+		downloadURLPath = "https://speed.hetzner.de/1GB.bin"
+	} else if currentHour == 3 {
+		downloadURLPath = "https://fastest.fish/lib/downloads/1GiB.bin"
+	} else if currentHour == 4 {
+		downloadURLPath = "http://speedtest-nyc1.digitalocean.com/1gb.test"
+	} else if currentHour == 5 {
+		downloadURLPath = "http://speedtest-ams2.digitalocean.com/1gb.test"
+	} else if currentHour == 6 {
+		downloadURLPath = "http://speedtest-sfo1.digitalocean.com/1gb.test"
+	} else if currentHour == 7 {
+		downloadURLPath = "http://speedtest-sgp1.digitalocean.com/1gb.test"
+	} else if currentHour == 8 {
+		downloadURLPath = "http://speedtest-lon1.digitalocean.com/1gb.test"
+	} else if currentHour == 9 {
+		downloadURLPath = "https://ga-us-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 10 {
+		downloadURLPath = "https://il-us-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 11 {
+		downloadURLPath = "https://tx-us-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 12 {
+		downloadURLPath = "https://lax-ca-us-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 13 {
+		downloadURLPath = "https://fl-us-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 14 {
+		downloadURLPath = "https://ams-nl-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 15 {
+		downloadURLPath = "https://tor-ca-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 16 {
+		downloadURLPath = "https://sjo-ca-us-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 17 {
+		downloadURLPath = "https://lax-ca-us-ping.vultr.com/vultr.com.1000MB.bin"
+	} else if currentHour == 18 {
+		downloadURLPath = "http://speedtest-sfo2.digitalocean.com/1gb.test"
+	} else if currentHour == 19 {
+		downloadURLPath = "http://speedtest-nyc3.digitalocean.com/1gb.test"
+	} else if currentHour == 20 {
+		downloadURLPath = "http://speedtest-blr1.digitalocean.com/1gb.test"
+	} else if currentHour == 21 {
+		downloadURLPath = "http://speedtest-sfo3.digitalocean.com/1gb.test"
+	} else if currentHour == 22 {
+		downloadURLPath = "http://speedtest-tor1.digitalocean.com/1gb.test"
+	} else if currentHour == 23 {
+		downloadURLPath = "https://speed.hetzner.de/1GB.bin"
+	} else if currentHour == 24 {
+		downloadURLPath = "https://syd-au-ping.vultr.com/vultr.com.1000MB.bin"
+	}
+	return downloadURLPath
 }
