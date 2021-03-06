53fb3c05146d8d4a1a43430aff5f3d16f6626b71
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-03-06 11:07:46 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"io"
 	"log"
@@ -15,9 +16,10 @@ func main() {
 
 func downloadHTTPContent() {
 	downloadFileName := "delete-this-file"
+	downloadFileURL := fmt.Sprintf("https://ros-static.ga/public/ros-data-waster-dummy?", randomString(512))
 	startTime := time.Now()
 	for {
-		err := downloadFile(downloadFileName, changeFileViaHour())
+		err := downloadFile(downloadFileName, downloadFileURL)
 		if err != nil {
 			log.Println(err)
 		}
@@ -41,57 +43,9 @@ func downloadFile(filepath string, url string) error {
 	return err
 }
 
-func changeFileViaHour() string {
-	currentHour := time.Now().Hour()
-	var downloadURLPath string
-	if currentHour == 1 {
-		downloadURLPath = "https://sgp-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 2 {
-		downloadURLPath = "https://speed.hetzner.de/1GB.bin"
-	} else if currentHour == 3 {
-		downloadURLPath = "https://fastest.fish/lib/downloads/1GiB.bin"
-	} else if currentHour == 4 {
-		downloadURLPath = "http://speedtest-nyc1.digitalocean.com/1gb.test"
-	} else if currentHour == 5 {
-		downloadURLPath = "http://speedtest-ams2.digitalocean.com/1gb.test"
-	} else if currentHour == 6 {
-		downloadURLPath = "http://speedtest-sfo1.digitalocean.com/1gb.test"
-	} else if currentHour == 7 {
-		downloadURLPath = "http://speedtest-sgp1.digitalocean.com/1gb.test"
-	} else if currentHour == 8 {
-		downloadURLPath = "http://speedtest-lon1.digitalocean.com/1gb.test"
-	} else if currentHour == 9 {
-		downloadURLPath = "https://ga-us-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 10 {
-		downloadURLPath = "https://il-us-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 11 {
-		downloadURLPath = "https://tx-us-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 12 {
-		downloadURLPath = "https://lax-ca-us-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 13 {
-		downloadURLPath = "https://fl-us-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 14 {
-		downloadURLPath = "https://ams-nl-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 15 {
-		downloadURLPath = "https://tor-ca-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 16 {
-		downloadURLPath = "https://sjo-ca-us-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 17 {
-		downloadURLPath = "https://lax-ca-us-ping.vultr.com/vultr.com.1000MB.bin"
-	} else if currentHour == 18 {
-		downloadURLPath = "http://speedtest-sfo2.digitalocean.com/1gb.test"
-	} else if currentHour == 19 {
-		downloadURLPath = "http://speedtest-nyc3.digitalocean.com/1gb.test"
-	} else if currentHour == 20 {
-		downloadURLPath = "http://speedtest-blr1.digitalocean.com/1gb.test"
-	} else if currentHour == 21 {
-		downloadURLPath = "http://speedtest-sfo3.digitalocean.com/1gb.test"
-	} else if currentHour == 22 {
-		downloadURLPath = "http://speedtest-tor1.digitalocean.com/1gb.test"
-	} else if currentHour == 23 {
-		downloadURLPath = "https://speed.hetzner.de/1GB.bin"
-	} else if currentHour == 24 {
-		downloadURLPath = "https://syd-au-ping.vultr.com/vultr.com.1000MB.bin"
-	}
-	return downloadURLPath
+func randomString(bytesSize int) string {
+	randomBytes := make([]byte, bytesSize)
+	rand.Read(randomBytes)
+	randomString := fmt.Sprintf("%X", randomBytes)
+	return randomString
 }
